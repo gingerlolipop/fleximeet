415b6bf6d5d0d6c05081938c2a5ea696bbb3cf4f
gingerlolipop <jillblack@qq.com>
2023-12-02 15:39:47 -0800

    fixed date shifting issue and consecutive hours

--- script222.js
+++ script222.js
@@ -3,20 +3,23 @@ let userPasswords = {};
 
 function generateCalendar() {
     const calendar = document.getElementById('calendar');
-    // Corrected dates, including the correct weekdays
+    console.log("Generating calendar...");
+
     const dates = [
-        '2024-01-11', // January 11, 2024 (Thursday)
-        '2024-01-12', // January 12, 2024 (Friday)
-        '2024-03-01', '2024-03-04', '2024-03-05', '2024-03-06', '2024-03-07', '2024-03-08', // March 1 and 4-8, 2024 (Weekdays)
-        '2024-03-11', '2024-03-12', '2024-03-13', '2024-03-14'  // March 11-14, 2024 (Weekdays)
+        '2024-01-11', '2024-01-12', // January 11-12, 2024
+        '2024-02-26', '2024-02-27', '2024-02-28', '2024-02-29', // February 26-29, 2024
+        '2024-03-01', '2024-03-04', '2024-03-05', '2024-03-06', '2024-03-07', '2024-03-08', '2024-03-11', '2024-03-12', // March 1-12, 2024
     ];
-
+    
     const times = [];
-    // Generate 30-minute time slots from 9:00 AM to 6:30 PM
-    for (let hour = 9; hour < 19; hour++) {
-        times.push(`${hour}:00`, `${hour}:30`);
+    // Generate 30-minute time slots from 9:00 AM to 6:00 PM
+    for (let hour = 9; hour <= 18; hour++) {
+        times.push(`${hour}:00`);
+        if (hour < 18) {
+            times.push(`${hour}:30`);
+        }
     }
-
+    
     // Create table and header row
     const table = document.createElement('table');
     const headerRow = document.createElement('tr');
@@ -24,15 +27,17 @@ function generateCalendar() {
     headerRow.appendChild(firstCell);
 
     dates.forEach(dateString => {
-        const date = new Date(dateString);
+        // Split the date string into components
+        const [year, month, day] = dateString.split('-').map(num => parseInt(num));
+        // Create a new Date object using local time zone
+        const date = new Date(year, month - 1, day);
         const dateHeader = document.createElement('th');
         dateHeader.textContent = `${date.toLocaleDateString('en-US', { weekday: 'short', month: 'short', day: 'numeric' })}`;
         headerRow.appendChild(dateHeader);
 
-        // Log the date string and its corresponding Date object
-        console.log(dateString, date.toString());
+        console.log("Date string:", dateString, "Date object:", date.toString());
     });
-    
+
     table.appendChild(headerRow);
 
     // Create rows for each time slot
@@ -54,8 +59,8 @@ function generateCalendar() {
     });
 
     calendar.appendChild(table);
+    console.log("Calendar generated.");
 }
-
 
 function setupDragSelect() {
     let isSelecting = false;
@@ -64,15 +69,20 @@ function setupDragSelect() {
         if (e.target.classList.contains('time-slot')) {
             isSelecting = true;
             toggleSelect(e.target);
+            console.log("Selection started at:", e.target.dataset.date, e.target.dataset.time);
         }
     });
     calendar.addEventListener('mouseover', e => {
         if (isSelecting && e.target.classList.contains('time-slot')) {
             toggleSelect(e.target);
+            console.log("Selecting:", e.target.dataset.date, e.target.dataset.time);
         }
     });
     document.addEventListener('mouseup', () => {
-        isSelecting = false;
+        if (isSelecting) {
+            console.log("Selection ended.");
+            isSelecting = false;
+        }
     });
 
     function toggleSelect(element) {
@@ -93,44 +103,68 @@ document.getElementById('submit').addEventListener('click', () => {
         return;
     }
 
-    // Check for existing username and password match
     if (userPasswords[userName] && userPasswords[userName] !== password) {
         alert("Username already exists with a different password. Please use a different username or enter the correct password.");
         return;
     }
 
     const selectedSlots = Array.from(document.querySelectorAll('.selected'));
-    const availability = selectedSlots.map(slot => `${slot.dataset.date} ${slot.dataset.time}`);
-
-    // Check if the selected slots for each day total at least 2.5 hours
-    const isValidAvailability = validateAvailability(availability);
-    if (!isValidAvailability) {
-        alert("Please select time slots totaling at least 2.5 hours for each day.");
+    if (!isValidSelection(selectedSlots)) {
+        alert("Please select at least 2.5 consecutive hours.");
         return;
     }
 
+    const availability = selectedSlots.map(slot => `${slot.dataset.date} ${slot.dataset.time}`);
     allAvailability[userName] = availability;
-    userPasswords[userName] = password; // Store or update the password
+    userPasswords[userName] = password;
 
+    console.log("User submitted availability:", userName, availability);
     displayAllAvailability();
     clearSelection();
 });
 
-// Function to validate if the selected slots for each day total at least 2.5 hours
-function validateAvailability(availability) {
-    const timeSlotsByDate = availability.reduce((acc, slot) => {
-        const [date, time] = slot.split(' ');
-        acc[date] = acc[date] || [];
-        acc[date].push(time);
+function isValidSelection(selectedSlots) {
+    // Group slots by date
+    const slotsByDate = selectedSlots.reduce((acc, slot) => {
+        const date = slot.dataset.date;
+        if (!acc[date]) {
+            acc[date] = [];
+        }
+        acc[date].push(slot.dataset.time);
         return acc;
     }, {});
 
-    return Object.values(timeSlotsByDate).every(slots => {
-        if (slots.length < 5) return false; // Each slot is 30 minutes, so we need at least 5 slots for 2.5 hours
-        // Additional logic can be added here to check for continuous time slots if needed
-        return true;
+    // Check each date for a continuous selection of at least 2.5 hours
+    return Object.values(slotsByDate).every(dateSlots => {
+        // Sort times for each date
+        dateSlots.sort();
+        let continuousBlock = 0; // To track continuous time blocks
+
+        for (let i = 0; i < dateSlots.length; i++) {
+            // Calculate the time difference between adjacent slots
+            if (i < dateSlots.length - 1) {
+                const currentTime = convertToMinutes(dateSlots[i]);
+                const nextTime = convertToMinutes(dateSlots[i + 1]);
+                if (nextTime - currentTime === 30) {
+                    continuousBlock += 30; // Increment the block size
+                    if (continuousBlock >= 120) { // Check if the block is at least 2.5 hours
+                        return true;
+                    }
+                } else {
+                    continuousBlock = 0; // Reset if the block is interrupted
+                }
+            }
+        }
+        return false; // No continuous block of at least 2.5 hours found
     });
 }
+
+function convertToMinutes(time) {
+    const [hours, minutes] = time.split(':').map(Number);
+    return hours * 60 + minutes;
+}
+
+
 function displayAllAvailability() {
     const resultsDiv = document.getElementById('availability-results');
     resultsDiv.innerHTML = '<h3>Availability</h3>';
@@ -153,11 +187,11 @@ function displayAllAvailability() {
 function clearSelection() {
     document.querySelectorAll('.selected').forEach(slot => slot.classList.remove('selected'));
     document.getElementById('username').value = ''; // Clear the name input
+    console.log("Selection cleared.");
 }
 
-// Admin functionality
 function adminDeleteRecord() {
-    const adminUsername = 'gingerlolipop';
+    const adminUsername = 'gingerlolipop'; // Example admin username
     const adminPassword = prompt('Enter admin password:');
     if (adminPassword === 'jingexcellent') { // Replace with a secure password
         const usernameToDelete = prompt('Enter username to delete:');
@@ -166,13 +200,13 @@ function adminDeleteRecord() {
             delete userPasswords[usernameToDelete];
             displayAllAvailability();
             alert(`Record for '${usernameToDelete}' has been deleted.`);
+            console.log(`Admin deleted record for: ${usernameToDelete}`);
         } else {
             alert("Username not found or invalid admin password.");
         }
     }
 }
 
-// Listen for a specific key combination (e.g., Ctrl+Shift+D)
 document.addEventListener('keydown', (event) => {
     if (event.ctrlKey && event.shiftKey && event.key === 'D') {
         adminDeleteRecord();
